70d1b88ded6f148a9e04c89421969994c6eb17d0
mmsw1597 <mmsw1597@naver.com>
2023-08-08 05:28:22 +0900

    Create Todo List

---
+++ src/components/TodoList.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import { debounce } from 'lodash'
+import Sortable from 'sortablejs'
+import { useTodosStore } from '~/store/todos'
+import TodoItem from '~/components/TodoItem.vue'
+import TheIcon from '~/components/TheIcon.vue'
+import TheBtn from './TheBtn.vue'
+
+const todosStore = useTodosStore()
+const todoListEl = ref<HTMLDivElement | null>(null)
+
+const debounced = debounce((value: boolean) => {
+  todosStore.updateCheckboxes(value)
+}, 400)
+
+const isAllChecked = computed({
+  get() {
+    return (
+      !!todosStore.filteredTodos.length &&
+      todosStore.filteredTodos.every((todo) => todo.done)
+    )
+  },
+  set(value: boolean) {
+    todosStore.todos.forEach((todo) => {
+      todo.done = value
+    })
+    debounced(value)
+  }
+})
+
+todosStore.fetchTodos()
+
+onMounted(() => {
+  initSortable()
+})
+
+function toggleAllCheckboxes() {
+  isAllChecked.value = !isAllChecked.value
+}
+function initSortable() {
+  if (todoListEl.value) {
+    new Sortable(todoListEl.value, {
+      handle: '.drag-handle',
+      animation: 0,
+      forceFallback: true,
+      onEnd: (event) => {
+        const { oldIndex, newIndex } = event
+        todosStore.reorderTodos({
+          oldIndex: oldIndex as number,
+          newIndex: newIndex as number
+        })
+      }
+    })
+  }
+}
+</script>
+
+<template>
+  <div class="todos-wrap shadow">
+    <div class="todo-head">
+      <TheIcon
+        :active="isAllChecked"
+        @click="toggleAllCheckboxes">
+        done_all
+      </TheIcon>
+      <div class="btn-group">
+        <TheBtn
+          v-for="filter in todosStore.filters"
+          :key="filter.name"
+          :active="todosStore.filterStatus === filter.name"
+          class="btn"
+          @click="todosStore.filterStatus = filter.name">
+          {{ filter.label }}
+        </TheBtn>
+        <TheBtn
+          danger
+          @click="todosStore.deleteDoneTodos">
+          완료만 삭제
+        </TheBtn>
+      </div>
+    </div>
+    <div
+      ref="todoListEl"
+      class="todo-list">
+      <TodoItem
+        v-for="todo in todosStore.filteredTodos"
+        :key="todo.id"
+        :todo="todo" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.todos-wrap {
+  border-radius: 6px;
+  overflow: hidden;
+}
+.todo-head {
+  height: 60px;
+  border-bottom: 1px solid var(--border-color);
+  background-color: rgba(#fff, 0.9);
+  position: relative;
+  :deep(.the-icon) {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: 24px;
+  }
+  .btn-group {
+    height: 100%;
+    display: flex;
+    position: absolute;
+    right: 0;
+  }
+}
+</style>
--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,19 +1,137 @@
 import axios from 'axios'
 import { defineStore } from 'pinia'
 
+export type Todos = Todo[]
+export interface Todo {
+  id: string
+  order: number
+  title: string
+  done: boolean
+  createdAt: string
+  updatedAt: string
+}
+
+type FilterStatus = 'all' | 'todo' | 'done'
+type Filters = Filter[]
+interface Filter {
+  label: string
+  name: FilterStatus
+}
+
 interface CreateTodoPayload {
   title: string
 }
 
+interface ReorderTodosPayload {
+  oldIndex: number
+  newIndex: number
+}
+
+const filters: Filters = [
+  { label: '전체', name: 'all' },
+  { label: '할 일만', name: 'todo' },
+  { label: '완료만', name: 'done' }
+]
+
 export const useTodosStore = defineStore('todos', {
-  state: () => ({}),
-  getters: {},
+  state: () => ({
+    todos: [] as Todos,
+    filterStatus: 'all' as FilterStatus,
+    filters
+  }),
+  getters: {
+    filteredTodos(state) {
+      return state.todos.filter((todo) => {
+        switch (state.filterStatus) {
+          case 'todo':
+            return !todo.done
+          case 'done':
+            return todo.done
+          case 'all':
+          default:
+            return true
+        }
+      })
+    }
+  },
   actions: {
+    async fetchTodos() {
+      const { data } = await axios.post('/api/todos', {
+        method: 'GET'
+      })
+      this.todos = data
+    },
     async createTodo({ title }: CreateTodoPayload) {
-      await axios.post('/api/todos', {
-        method: 'POST',
+      try {
+        const { data: createdTodo } = await axios.post('/api/todos', {
+          method: 'POST',
+          data: {
+            title
+          }
+        })
+        this.todos.unshift(createdTodo)
+      } catch (error) {
+        console.error('createdTodo: ', error)
+      }
+    },
+    async updateTodo(todo: Todo) {
+      const foundTodo = this.todos.find((t) => t.id === todo.id)
+      if (!foundTodo) return
+      const backedUpTodo = { ...foundTodo }
+      Object.assign(foundTodo, todo)
+
+      try {
+        const { id: path, title, done } = todo
+        const { data: updatedTodo } = await axios.post('/api/todos', {
+          method: 'PUT',
+          path,
+          data: {
+            title,
+            done
+          }
+        })
+      } catch (error) {
+        console.error('updateTodo: ', error)
+        Object.assign(foundTodo, backedUpTodo)
+      }
+    },
+    updateCheckboxes(done: boolean) {
+      this.todos.forEach((todo) => {
+        this.updateTodo({
+          ...todo,
+          done
+        })
+      })
+    },
+    async deleteDoneTodos() {
+      const todoIds = this.todos
+        .filter((todo) => todo.done)
+        .map((todo) => todo.id)
+      if (!todoIds.length) return
+
+      try {
+        await axios.post('/api/todos', {
+          method: 'DELETE',
+          path: 'deletions',
+          data: {
+            todoIds
+          }
+        })
+        this.todos = this.todos.filter((todo) => !todoIds.includes(todo.id))
+      } catch (error) {
+        console.error('deleteDoneTodos:', error)
+      }
+    },
+    reorderTodos({ oldIndex, newIndex }: ReorderTodosPayload) {
+      if (oldIndex === newIndex) return
+      const movedTodo = this.todos.splice(oldIndex, 1)[0]
+      this.todos.splice(newIndex, 0, movedTodo)
+      const todoIds = this.todos.map((todo) => todo.id)
+      axios.post('/api/todos', {
+        method: 'PUT',
+        path: 'reorder',
         data: {
-          title
+          todoIds
         }
       })
     }
