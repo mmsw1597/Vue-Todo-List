1c8da776ca79471334120d360e433a4902619bd4
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-08-10 12:32:33 +0900

    로컬서버 병렬실행 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
+  },
+  server: {
+    proxy: {
+      '/api': { target: 'http://localhost:2999' }
+    }
   }
 })
